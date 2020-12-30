3a4f6339c1e8e21160f23c225990293e5bfcbc8f
gitaudo <gabrieleghiba@icloud.com>
2020-12-30 16:01:47 +0100

    Fixed camera movement and added reticle

--- src/Rayc.cpp
+++ src/Rayc.cpp
@@ -9,8 +9,8 @@ Rayc::Rayc()
 
     frame_count = 0;
     
-    window_width = 1024;
-    window_height = 512;
+    window_width = 1536;
+    window_height = 768;
     
     map_width = 10;
     map_height = 10;
@@ -19,7 +19,7 @@ Rayc::Rayc()
 
     camera_x = 100;
     camera_y = 100;
-    camera_direction = 7.00;
+    camera_direction = 0.00;
     camera_fov = 1.00;
 
     framebuffer = new uint32_t[window_width*window_height];    
@@ -105,23 +105,24 @@ void Rayc::OnEvent(SDL_Event *event)
         switch(event->key.keysym.sym)
         {
         case SDLK_w:
-            camera_y += 2;
+            camera_x += 3*cos(camera_direction);
+            camera_y += 3*sin(camera_direction);
             break;
         case SDLK_a:
             camera_x += 2;
             break;
         case SDLK_s:
-            camera_y -= 2;
+            camera_x -= 3*cos(camera_direction);
+            camera_y -= 3*sin(camera_direction);
             break;
         case SDLK_d:
             camera_x -= 2;
             break;
         }
-
     }
     else if(event->type == SDL_MOUSEMOTION)
     {
-        camera_direction += event->motion.xrel * 0.005;
+        camera_direction += event->motion.xrel * 0.01;
     }
 }
 
@@ -154,9 +155,9 @@ void Rayc::OnRender()
     draw_circle(framebuffer, window_width, window_height, camera_x, camera_y, 10);
 
     // cast a ray in the camera looking direction
-    for(float v=camera_direction; v<camera_direction+camera_fov; v+=0.002)
+    for(float v=camera_direction-camera_fov/2; v<camera_direction+camera_fov/2; v+=0.001)
     {
-        for(float i=0; i<600; i+=0.5)
+        for(float i=0; i<800; i+=1)
         {
             float x_pos = camera_x + i*cos(v);
             float y_pos = camera_y + i*sin(v);
@@ -164,13 +165,17 @@ void Rayc::OnRender()
             {
                 size_t height = window_height/(i*0.02);
                 draw_rect(framebuffer, window_width, window_height,
-                          window_width/2+(v-camera_direction)*(500),
-                          window_height/2-height/2, 1, height, pack_rgb((height/(window_height/1.5))*255, 60, 0));
+                          window_width/2+(v-camera_direction+camera_fov/2)*(750),
+                          window_height/2-height/2, 1.5, height, pack_rgb((height/(window_height/1.5))*255, 60, 0));
                 break;
             }
-            framebuffer[int(x_pos)+int(y_pos)*(window_width)] = pack_rgb(255, 255, 255);
+            //framebuffer[int(x_pos)+int(y_pos)*(window_width)] = pack_rgb(255, 255, 255);
         }
     }
+
+    // draw reticle
+    draw_rect(framebuffer, window_width, window_height,
+              window_width*3/4, window_height/2-5, 1, 10);
     
     SDL_UpdateTexture(texture, NULL, framebuffer, window_width*sizeof(uint32_t));
     
